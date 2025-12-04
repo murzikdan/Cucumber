a745db124b80d611cc6d1bb3f1f18300d6fa56ee
ReserveBot <211949879+ReserveBot@users.noreply.github.com>
2025-12-04 21:33:53 +0000

    chore: Automatically update REUSE headers

--- Content.Client/UserInterface/Systems/Chat/ChatTransliterationSystem.cs
+++ Content.Client/UserInterface/Systems/Chat/ChatTransliterationSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Space Station 14 Contributors
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
